b8221cc29497d464230789ef3712938963c14faa
maolin <lmao@glitech.com>
2021-07-05 11:26:21 +0800

    完善识别人脸

--- examples/04_skill/01_detect_face.py
+++ examples/04_skill/01_detect_face.py
@@ -1,13 +1,30 @@
 from gomerx import robot
+import cv2 as cv
+import time
 
 if __name__ == '__main__':
-    robot_name = 'GomerX_6e09ba'
+    robot_name = 'GomerX_SkOhHU'
     my_robot = robot.Robot(robot_name)
+    my_camera = my_robot.camera
     my_skill = my_robot.skill
+    my_camera.start_video_stream(display=False)
+    while True:
 
-    result = my_skill.detect_face()
-    if result:
-        print("Found face")
-    else:
-        print("Not Found face")
+        result = my_skill.detect_face()
+        img = my_camera.read_cv_image()
+        if img is None:
+            continue
+        if result:
+            print("Found face")
+            cv.putText(img, 'face', (0, 100), fontFace=cv.FONT_HERSHEY_SIMPLEX,
+                       fontScale=3, color=(0, 255, 0), thickness=5)
+        else:
+            print("Not Found face")
+            cv.putText(img, 'no face', (0, 100), fontFace=cv.FONT_HERSHEY_SIMPLEX,
+                       fontScale=3, color=(0, 0, 255), thickness=5)
+        cv.imshow('win', img)
+        if cv.waitKey(20) == 27:
+            break
+    cv.destroyAllWindows()
+    my_camera.stop_video_stream()
     my_robot.close()
