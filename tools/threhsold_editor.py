5fd7dfc9b8f4b6f5f8f6cb01a74bce077b6118f7
zixuan <461387340@qq.com>
2021-07-08 14:22:35 +0800

    完成格式修改

--- tools/threhsold_editor.py
+++ tools/threhsold_editor.py
@@ -3,54 +3,84 @@ import numpy as np
 from gomerx import robot
 
 
+# 滑动条的回调函数，获取滑动条位置处的值
+def get_track_bar_value(*args):
+    h_min = cv.getTrackbarPos(track_bar_h_min, window_name)
+    h_max = cv.getTrackbarPos(track_bar_h_max, window_name)
+    s_min = cv.getTrackbarPos(track_bar_s_min, window_name)
+    s_max = cv.getTrackbarPos(track_bar_s_max, window_name)
+    v_min = cv.getTrackbarPos(track_bar_v_min, window_name)
+    v_max = cv.getTrackbarPos(track_bar_v_max, window_name)
+
+    # 值的重映射
+    h_min, h_max = 0.5*h_min, 0.5*h_max
+    s_min, s_max = s_min*2.55, s_max*2.55
+    v_min, v_max = v_min*2.55, v_max*2.55
+
+    return h_min, h_max, s_min, s_max, v_min, v_max
+
+
 if __name__ == '__main__':
     robot_name = 'GomerX_6e09ba'
     my_robot = robot.Robot(robot_name)
     my_camera = my_robot.camera
     my_camera.start_video_stream(display=False)
+
+    # 确保得到视频流之后再显示窗口显示图片
     while True:
         img = my_camera.read_cv_image()
         if img is not None:
             break
 
-    # 滑动条的回调函数，获取滑动条位置处的值
-    def empty(a):
-        h_min = cv.getTrackbarPos("Hue Min", "TrackBars")
-        h_max = cv.getTrackbarPos("Hue Max", "TrackBars")
-        s_min = cv.getTrackbarPos("Sat Min", "TrackBars")
-        s_max = cv.getTrackbarPos("Sat Max", "TrackBars")
-        v_min = cv.getTrackbarPos("Val Min", "TrackBars")
-        v_max = cv.getTrackbarPos("Val Max", "TrackBars")
-        return h_min, h_max, s_min, s_max, v_min, v_max
+    window_name = "TrackBars"
+    track_bar_h_max = "H_max"
+    track_bar_h_min = "H_min"
+    track_bar_s_max = "S_max"
+    track_bar_s_min = "S_min"
+    track_bar_v_max = "V_max"
+    track_bar_v_min = "V_min"
 
     # 创建一个窗口，放置6个滑动条
-    cv.namedWindow("TrackBars", 0)
-    cv.resizeWindow('TrackBars', 900, 800)
-    cv.createTrackbar("Hue Min", "TrackBars", 50, 179, empty)
-    cv.createTrackbar("Hue Max", "TrackBars", 100, 179, empty)
-    cv.createTrackbar("Sat Min", "TrackBars", 100, 255, empty)
-    cv.createTrackbar("Sat Max", "TrackBars", 180, 255, empty)
-    cv.createTrackbar("Val Min", "TrackBars", 50, 255, empty)
-    cv.createTrackbar("Val Max", "TrackBars", 225, 255, empty)
+    # h_min, h_max 范围 (0-360)
+    # s_min, s_max, v_min, v_max 范围 (0-100)
+
+    cv.namedWindow(window_name, 0)
+    cv.resizeWindow(window_name, 1000, 800)
+    cv.createTrackbar(track_bar_h_min, window_name,
+                      97, 360, get_track_bar_value)
+    cv.createTrackbar(track_bar_h_max, window_name,
+                      232, 360, get_track_bar_value)
+    cv.createTrackbar(track_bar_s_min, window_name,
+                      30, 100, get_track_bar_value)
+    cv.createTrackbar(track_bar_s_max, window_name,
+                      60, 100, get_track_bar_value)
+    cv.createTrackbar(track_bar_v_min, window_name,
+                      35, 100, get_track_bar_value)
+    cv.createTrackbar(track_bar_v_max, window_name,
+                      60, 100, get_track_bar_value)
 
     while True:
         img = my_camera.read_cv_image()
-        imgHSV = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+        img_HSV = cv.cvtColor(img, cv.COLOR_BGR2HSV)
 
         # 调用回调函数，获取滑动条的值
-        h_min, h_max, s_min, s_max, v_min, v_max = empty(0)
+        h_min, h_max, s_min, s_max, v_min, v_max = get_track_bar_value(0)
         lower = np.array([h_min, s_min, v_min])
         upper = np.array([h_max, s_max, v_max])
 
         # 获得指定颜色范围内的掩码
-        mask = cv.inRange(imgHSV, lower, upper)
+        mask = cv.inRange(img_HSV, lower, upper)
 
         # 单通道扩展为三通道
         mask = cv.cvtColor(mask, cv.COLOR_GRAY2BGR)
 
         # 对阈值处理后的图和原图进行拼接
         composite_image = np.hstack((img, mask))
-        cv.imshow("TrackBars", composite_image)
+
+        # 显示最终结果
+        cv.imshow(window_name, composite_image)
+
+        # 按下 Esc 键退出程序
         if cv.waitKey(1) & 0xFF == 27:
             break
 
