dd87b0fe07f37882ae7ceb64ff6252ad7011f67b
zixuan <461387340@qq.com>
2021-07-19 09:54:01 +0800

    修复格式错误

--- examples/05_opencv/02_draw_picture.py
+++ examples/05_opencv/02_draw_picture.py
@@ -10,25 +10,26 @@ if __name__ == '__main__':
     my_camera.start_video_stream(display=False)
 
     print('按 ESC 键退出')
-    
+
     while True:
         img = my_camera.read_cv_image()
         if img is not None:
-            h,w = img.shape[0], img.shape[1]
+            h, w = img.shape[0], img.shape[1]
 
             # 画一条直线
-            x1,y1 = int(h/5), int(w/2)
-            x2,y2 = int(h/5), int(w/4)
-            img = cv.line(img, (x1, y1), (x2, y2), color=(255, 0, 0), thickness=2)
+            x1, y1 = int(h/5), int(w/2)
+            x2, y2 = int(h/5), int(w/4)
+            img = cv.line(img, (x1, y1), (x2, y2),
+                          color=(255, 0, 0), thickness=2)
 
             # 画一个矩形
-            x1,y1 = int(h/4), int(w/2)
-            x2,y2 = int(h/2), int(w/4)
+            x1, y1 = int(h/4), int(w/2)
+            x2, y2 = int(h/2), int(w/4)
             img = cv.rectangle(img, (x1, y1), (x2, y2),
-                            color=(0, 0, 255), thickness=1)
-            
+                               color=(0, 0, 255), thickness=1)
+
             # 画一个圆
-            x1,y1 = int(h/2)+100, int(w/2)-100
+            x1, y1 = int(h/2)+100, int(w/2)-100
             r = int(w/12)
             img = cv.circle(img, (x1, y1), r, color=(0, 255, 0), thickness=2)
 
@@ -36,7 +37,6 @@ if __name__ == '__main__':
             if cv.waitKey(10) == 27:
                 cv.destroyAllWindows()
                 break
-            
-        
+
     my_camera.stop_video_stream()
     my_robot.close()
