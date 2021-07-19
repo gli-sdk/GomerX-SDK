1cd2915e4e02ff1e23af06e3341a58b332d75cd9
zixuan <461387340@qq.com>
2021-07-19 09:41:31 +0800

    修复opencv例程bug

--- examples/05_opencv/01_read_save_image.py
+++ examples/05_opencv/01_read_save_image.py
@@ -6,22 +6,21 @@ if __name__ == '__main__':
     my_robot = robot.Robot(robot_name)
     my_skill = my_robot.skill
     my_camera = my_robot.camera
+    my_camera.start_video_stream(display=False)
 
-    is_open = my_camera.start_video_stream(display=False)
-    if is_open == True:
-        print('视频流已打开')
-        print('按下数字键 1，退出显示。\n按下数字键 2，保存图片。')
-        while True:
-            img = my_camera.read_cv_image()
+    print('按下数字键 1, 退出显示。\n按下数字键 2, 保存图片。')
+    image_num = 1
+    while True:
+        img = my_camera.read_cv_image()
+        if img is not None:
             cv.imshow('img', img)
-            key = cv.waitKey(50)
+            key = cv.waitKey(1)
             if key == ord('1'):
                 break
             elif key == ord('2'):
-                cv.imwrite('img.jpg', img)
-                break
-    else:
-        print('视频流未打开')
+                image_name = 'img' + str(image_num) + '.jpg'
+                cv.imwrite(image_name, img)
+                image_num = image_num + 1
 
     my_camera.stop_video_stream()
     my_robot.close()
--- examples/05_opencv/02_draw_picture.py
+++ examples/05_opencv/02_draw_picture.py
@@ -1,20 +1,21 @@
 from gomerx import robot
 import cv2 as cv
 import numpy as np
+import time
 
 if __name__ == '__main__':
     robot_name = 'GomerX_6e09ba'
     my_robot = robot.Robot(robot_name)
     my_camera = my_robot.camera
+    my_camera.start_video_stream(display=False)
 
-    is_open = my_camera.start_video_stream(display=False)
-    if is_open == True:
-        print('视频流已打开')
-        print('按 ESC 键退出')
-        while True:
-            img = my_camera.read_cv_image()
+    print('按 ESC 键退出')
+    
+    while True:
+        img = my_camera.read_cv_image()
+        if img is not None:
             h,w = img.shape[0], img.shape[1]
-            print(h,w)
+
             # 画一条直线
             x1,y1 = int(h/5), int(w/2)
             x2,y2 = int(h/5), int(w/4)
@@ -27,17 +28,15 @@ if __name__ == '__main__':
                             color=(0, 0, 255), thickness=1)
             
             # 画一个圆
-            x1,y1 = int(h/2), int(w/2)
-            r = int(w/6)
+            x1,y1 = int(h/2)+100, int(w/2)-100
+            r = int(w/12)
             img = cv.circle(img, (x1, y1), r, color=(0, 255, 0), thickness=2)
 
             cv.imshow('img', img)
             if cv.waitKey(10) == 27:
-                print('1')
                 cv.destroyAllWindows()
                 break
-    else:
-        print('视频流打开失败')
+            
         
     my_camera.stop_video_stream()
     my_robot.close()
